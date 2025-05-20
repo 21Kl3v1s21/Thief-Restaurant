1be67e74f27fe77c1ca3c4ea7e388fc8d38d44f6
21Kl3v1s21 <klevis25sfj@gmail.com>
2025-05-20 18:12:12 +0200

    Why become a superhero hm??

--- src/app/sections/WhyUs.tsx
+++ src/app/sections/WhyUs.tsx
@@ -2,28 +2,18 @@ import React from 'react'
 import './whyUs.css'
 import WhyUsCard from '../components/WhyUsCard'
 import SectionTitle from '../components/SectionTitle'
-
-type WhyUsItem = {
-  id: number
-  title: string
-  content: string
-}
-
-async function getWhyUsData() {
-  const res = await fetch('http://localhost:3000/api/whyus')
-  return res.json()
-}
+import { whyUs } from '../data/data'
 
 export default async function WhyUs() {
-  const items: WhyUsItem[] = await getWhyUsData()
 
   return (
-    <section id="why-us" className="why-us">
+     <section id="why-us" className="why-us">
       <div className="container">
         <SectionTitle title="Why Us" subtitle="Why Choose Our Restaurant" />
         <div className="row justify-content-center">
-          {items?.length > 0 &&
-            items.map((item) => <WhyUsCard key={item.id} item={item} />)}
+          {whyUs.map((item) => (
+            <WhyUsCard key={item.id} item={item} />
+          ))}
         </div>
       </div>
     </section>
