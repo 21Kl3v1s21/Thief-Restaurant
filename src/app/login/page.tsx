be90f709374d7ea4799708595744aa0034765f63
21Kl3v1s21 <klevis25sfj@gmail.com>
2025-05-22 19:41:26 +0200

    :)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,10 @@
 // app/login/page.tsx
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from '@clerk/nextjs';
+import './page.css';
 
 export default function LoginPage() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="log flex justify-center items-center min-h-screen">
       <SignIn 
         routing='hash'
         afterSignInUrl="/admin/bookings"
